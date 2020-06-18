a8670f35a4abb24b3d06bbd36ce9608b82dd3bb4
Fernando Vicente <fernando.limavicente@gmail.com>
2020-06-18 19:47:46 -0300

    Criado o manuseio de sessoes do sistema

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,17 +2,36 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer Id;
-	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
+
+	private LocalTime horario;
 
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
+	}
+
+	/**
+	 * @deprecated hibernate only
+	 */
+	public Sessao() {
+
 	}
 
 	public Integer getId() {
